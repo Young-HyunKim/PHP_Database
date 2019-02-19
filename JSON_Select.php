eea9e6eff3564c116de631f4649c3f0f7189bc21
Young-Hyun Kim <contact@younghyun.kr>
2019-02-19 21:23:22 +0900

    Add : JSON_Select.php

---
+++ JSON_Select.php
@@ -0,0 +1,11 @@
+<?php
+$conn = mysqli_connect('host','id','pw','DB_Name');
+$sql = "SELECT * FROM Table_Name";
+$row = mysqli_fetch_array($result);
+
+$row = mysqli_fetch_array($result);
+$JsonArray = array("number" => $row[0],"Title" => $row[1],"Content" => $row[2],"Date" => $row[3]); // Todo encoding.
+
+echo json_encode($JsonArray);
+
+?>
