65ccc0f1d1a31056323da656f035f8c85c1023f0
Young-Hyun Kim <contact@younghyun.kr>
2019-02-26 01:02:36 +0900

    Remove : '$set' Variable

--- JSON_Select.php
+++ JSON_Select.php
@@ -6,8 +6,6 @@ $row = mysqli_fetch_array($result);
 $row = mysqli_fetch_array($result);
 $JsonArray = array("number" => $row[0],"Title" => $row[1],"Content" => $row[2],"Date" => $row[3]); // Todo encoding.
 
-$set = json_encode($JsonArray,JSON_UNESCAPED_UNICODE);
-echo $set;
-
+echo json_encode($JsonArray,JSON_UNESCAPED_UNICODE);
 
 ?>
